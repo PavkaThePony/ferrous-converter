89193dc626a03cde4ef149534d6518a32ee52425
Pavka <null@null.equestria>
2026-07-31 19:54:24 +0700

    feat: Miles and Feet are convertable now

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ enum Action {
     KelToCel,
     NetherToOverworld,
     OverworldToNether,
+    FeetToMeters,
+    MilesToKms,
 }
 
 struct ConvResult {
@@ -27,20 +29,21 @@ fn main() {
     println!("║ Ferrous Converter                      ║");
     println!("╚════════════════════════════════════════╝");
     println!();
+    println!(" » Select conversion type(q to quit):");
+    println!("  [1] Hammer Unit     -> Centimeter");
+    println!("  [2] Centimeter      -> Hammer Unit");
+    println!("  [3] Celsius         -> Fahrenheit");
+    println!("  [4] Fahrenheit      -> Celsius");
+    println!("  [5] Celsius         -> Kelvin");
+    println!("  [6] Kelvin          -> Celsius");
+    println!("  [7] Overworld       -> Nether");
+    println!("  [8] Nether          -> Overworld");
+    println!("  [9] Feet            -> Meters");
+    println!("  [10] Miles          -> Kilometers");
     loop {
         #[allow(unused_assignments, unused_variables)]
         let action = Action::None;
         response.clear();
-        println!(" » Select conversion type(q to quit):");
-        println!("  [1] Hammer Unit     -> Centimeter");
-        println!("  [2] Centimeter      -> Hammer Unit");
-        println!("  [3] Celsius         -> Fahrenheit");
-        println!("  [4] Fahrenheit      -> Celsius");
-        println!("  [5] Celsius         -> Kelvin");
-        println!("  [6] Kelvin          -> Celsius");
-        println!("  [7] Overworld       -> Nether");
-        println!("  [8] Nether          -> Overworld");
-
         print!(" Enter index »» ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut response).expect("Something went wrong");
@@ -63,6 +66,8 @@ fn main() {
             6   => Action::KelToCel,
             7   => Action::OverworldToNether,
             8   => Action::NetherToOverworld,
+            9   => Action::FeetToMeters,
+            10  => Action::MilesToKms,
             _   => Action::None,
         };
         
@@ -73,27 +78,27 @@ fn main() {
             },
             Action::HuToCm => {
                 let value = get_single_value(&mut response);
-                hu_to_cm(&value);
+                hu_to_cm(value);
             }
             Action::CmToHu => {
                 let value = get_single_value(&mut response);
-                cm_to_hu(&value);
+                cm_to_hu(value);
             }
             Action::CelToFah => {
                 let value = get_single_value(&mut response);
-                c_to_f(&value);
+                c_to_f(value);
             }
             Action::FahToCel => {
                 let value = get_single_value(&mut response);
-                f_to_c(&value); 
+                f_to_c(value); 
             }
             Action::CelToKel => {
                 let value = get_single_value(&mut response);
-                c_to_k(&value);
+                c_to_k(value);
             }
             Action::KelToCel => {
                 let value = get_single_value(&mut response);
-                k_to_c(&value);
+                k_to_c(value);
             }
             Action::OverworldToNether => {
                 let coords = get_coordinates(&mut response);
@@ -102,6 +107,14 @@ fn main() {
             Action::NetherToOverworld => {
                 let coords = get_coordinates(&mut response);
                 nether_to_overworld(&coords);
+            }
+            Action::MilesToKms => {
+                let value = get_single_value(&mut response);
+                miles_to_kms(value);
+            }
+            Action::FeetToMeters => {
+                let value = get_single_value(&mut response);
+                feet_to_meters(value);
             }
         }
     }
@@ -194,7 +207,7 @@ fn print_result(res: &ConvResult){
     blank.clear();
 }
 
-fn hu_to_cm(hu: &f64) {
+fn hu_to_cm(hu: f64) {
     // 1 HU == 2.54 CM
     let result = hu * 2.54_f64;
     let res = ConvResult {
@@ -204,7 +217,7 @@ fn hu_to_cm(hu: &f64) {
     print_result(&res);
 }
 
-fn cm_to_hu(cm: &f64) {
+fn cm_to_hu(cm: f64) {
     // 1 HU == 2.54 CM
     let result = cm / 2.54_f64;
     let res = ConvResult {
@@ -213,7 +226,7 @@ fn cm_to_hu(cm: &f64) {
     };
     print_result(&res);
 }
-fn c_to_f(c: &f64) {
+fn c_to_f(c: f64) {
     // F = C * 1.8 + 32
     let result = c * 1.8_f64 + 32.0_f64;
     let res = ConvResult {
@@ -223,7 +236,7 @@ fn c_to_f(c: &f64) {
     print_result(&res);
 }
 
-fn f_to_c(f: &f64) {
+fn f_to_c(f: f64) {
     // C = (F - 32) / 1.8
     let result = (f - 32.0_f64) / 1.8_f64;
     let res = ConvResult {
@@ -233,7 +246,7 @@ fn f_to_c(f: &f64) {
     print_result(&res);
 }
 
-fn c_to_k(c: &f64) {
+fn c_to_k(c: f64) {
     // K = C + 273.15
     let result = c + 273.15_f64;
     let res = ConvResult {
@@ -243,7 +256,7 @@ fn c_to_k(c: &f64) {
     print_result(&res);
 }
 
-fn k_to_c(k: &f64) {
+fn k_to_c(k: f64) {
     // C = K - 273.15
     let result = k - 273.15_f64;
     let res = ConvResult {
@@ -283,4 +296,24 @@ fn print_result_mc_coordinates(coords: &McCoordinates) {
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut blank).expect("Couldn't read line");
     blank.clear();
+}
+
+fn feet_to_meters(feet: f64) {
+    // divide by 3.28084
+    let result = feet / 3.28084_f64;
+    let res = ConvResult {
+        value : result.to_string(),
+        type_m : String::from("Meters"),
+    };
+    print_result(&res);
+}
+
+fn miles_to_kms(miles: f64) {
+    // multiply by 1.60934
+    let result = miles * 1.60934;
+    let res = ConvResult {
+        value : result.to_string(),
+        type_m : String::from("KM"),
+    };
+    print_result(&res);
 }
